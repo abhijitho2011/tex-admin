717fbb3f3240f39ebe06e7af815e10c87ac8675a
abhijitho2011 <149400823+abhijitho2011@users.noreply.github.com>
2025-04-12 01:03:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import TrackCouriers from './pages/TrackCouriers';
-import ApprovePartners from './pages/ApprovePartners';
-import ManageHubs from './pages/ManageHubs';
-import ManagePricing from './pages/ManagePricing';
-import Login from './pages/Login';
-import Navbar from './components/Navbar';
+import React from "react";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import TrackCouriers from "./pages/TrackCouriers";
+import ApprovePartners from "./pages/ApprovePartners";
+import ManageHubs from "./pages/ManageHubs";
+import ManagePricing from "./pages/ManagePricing";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
-const App = () => {
+function App() {
+  const isLoggedIn = localStorage.getItem("adminToken");
+
   return (
     <Router>
-      <Navbar />
+      {isLoggedIn && <Navbar />}
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/couriers" element={<TrackCouriers />} />
-        <Route path="/partners" element={<ApprovePartners />} />
-        <Route path="/hubs" element={<ManageHubs />} />
-        <Route path="/pricing" element={<ManagePricing />} />
+        <Route path="/" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path="/track" element={isLoggedIn ? <TrackCouriers /> : <Navigate to="/login" />} />
+        <Route path="/partners" element={isLoggedIn ? <ApprovePartners /> : <Navigate to="/login" />} />
+        <Route path="/hubs" element={isLoggedIn ? <ManageHubs /> : <Navigate to="/login" />} />
+        <Route path="/pricing" element={isLoggedIn ? <ManagePricing /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
